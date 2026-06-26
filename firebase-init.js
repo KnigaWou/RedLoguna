2825aa21a4309d33d928585f626190ff1f7e4569
KnigaWou <bravenewfeast@gmail.com>
2026-06-26 17:34:25 +0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,13 @@
+// ============================================
+//  firebase-init.js — Инициализация Firebase
+// ============================================
 
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue, push, set, get } from "firebase/database";
+
+const firebaseConfig = CONFIG.firebase;
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, onValue, push, set, get };
